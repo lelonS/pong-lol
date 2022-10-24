bc4ee8a0f178bbcac7513bd7ea574f15ae76c8b5
Teddymannen <teddy_kross@hotmail.com>
2022-10-24 14:01:54 +0200

    move stuff!

--- classes/player.py
+++ classes/player.py
@@ -1,11 +1,26 @@
+import pygame
+
+
 class Player:
     x: int
     y: int
     life: int
     speed: int
+    border_y: int
 
     def __init__(self, x, y, life, speed):
         self.x = x
         self.y = y
         self.life = life
         self.speed = speed
+        self.border_y = 600
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, 10, 60))
+
+    def move(self, direction):
+        self.y += direction * self.speed
+        if self.y + 60 > self.border_y:
+            self.y = self.border_y - 60
+        if self.y < 0:
+            self.y = 0
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from classes.ball import Ball
+from classes.player import Player
 
 pygame.init()
 
@@ -8,7 +10,27 @@ SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def main():
-    input()
+    player1 = Player(50, 270, 10, 0.3)  # spawn player 1
+    player2 = Player(550, 270, 10, 0.3)  # spawn player 2
+    while True:
+        SCREEN.fill((0, 0, 0))
+        player1.draw(SCREEN)
+        player2.draw(SCREEN)
+        pygame.display.update()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player1.move(-1)
+        if keys[pygame.K_s]:
+            player1.move(1)
+        if keys[pygame.K_o]:
+            player2.move(-1)
+        if keys[pygame.K_l]:
+            player2.move(1)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # Close the program any way you want, or troll users who want to close your program.
+                raise SystemExit
 
 
 if __name__ == '__main__':
