71796a5eb3381b9a20db5536f8e1c1147c602d6b
lelonS <35070147+lelonS@users.noreply.github.com>
2022-10-24 13:24:11 +0200

    Update ball.py

--- classes/ball.py
+++ classes/ball.py
@@ -1,2 +1,22 @@
+import pygame
+from pygame import Vector2
+
+
 class Ball:
-    pass
+    pos: Vector2
+    direction: Vector2
+    velocity: float
+    radius: int = 10
+
+    def __init__(self, x: float, y: float, speed: float) -> None:
+        self.x = x
+        self.y = y
+        self.direction = Vector2().normalize()
+        self.velocity = speed
+
+    def update_pos(self):
+        self.pos += self.velocity * self.direction
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255, 255, 255),
+                           (self.pos.x, self.pos.y), self.radius)
