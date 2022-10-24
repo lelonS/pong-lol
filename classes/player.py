ae1cbbec6b1f6c6885b14bcb126bfc11e3fc45ff
Teddymannen <teddy_kross@hotmail.com>
2022-10-24 13:14:39 +0200

    began class Player

--- classes/player.py
+++ classes/player.py
@@ -1,2 +1,11 @@
 class Player:
-    pass
+    x: int
+    y: int
+    life: int
+    speed: int
+
+    def __init__(self, x, y, life, speed):
+        self.x = x
+        self.y = y
+        self.life = life
+        self.speed = speed
