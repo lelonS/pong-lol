8f0360fd56e7d35840536d031948d2fe0f45065d
lelonS <35070147+lelonS@users.noreply.github.com>
2022-10-25 15:56:43 +0200

    Add functions

--- main.py
+++ main.py
@@ -11,28 +11,37 @@ FPS = 60
 CLOCK = pygame.time.Clock()
 
 
+def draw(player1, player2, ball):
+    SCREEN.fill((0, 0, 0))
+    player1.draw(SCREEN)
+    player2.draw(SCREEN)
+    ball.draw(SCREEN)
+    pygame.display.update()
+
+
+def handle_keys_pressed(player1, player2):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player1.move(-1)
+    if keys[pygame.K_s]:
+        player1.move(1)
+    if keys[pygame.K_o]:
+        player2.move(-1)
+    if keys[pygame.K_l]:
+        player2.move(1)
+
+
 def main():
     player1 = Player(50, 270, 10, 3)  # spawn player 1
     player2 = Player(550, 270, 10, 3)  # spawn player 2
     ball = Ball(300, 300, 5)
     while True:
         CLOCK.tick(FPS)
-        SCREEN.fill((0, 0, 0))
-        player1.draw(SCREEN)
-        player2.draw(SCREEN)
-        ball.draw(SCREEN)
-        pygame.display.update()
+        draw(player1, player2, ball)
         ball.move()
         ball.collide((player1, player2))
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player1.move(-1)
-        if keys[pygame.K_s]:
-            player1.move(1)
-        if keys[pygame.K_o]:
-            player2.move(-1)
-        if keys[pygame.K_l]:
-            player2.move(1)
+
+        handle_keys_pressed(player1, player2)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
